1ae4ca0bc509bc71f8fd2799d171a98162ba6720
Mayles <oukacha.mayles@ynov.com>
2026-01-07 22:41:39 +0100

    Modification de import_data

--- sql/import_data.sql
+++ sql/import_data.sql
@@ -1,4 +1,4 @@
-CREATE TABLE vehicule_import (
+CREATE TABLE IF NOT EXISTS vehicule_import (
     id INT,
     marque TEXT,
     modele TEXT,
@@ -9,3 +9,15 @@ CREATE TABLE vehicule_import (
     etat TEXT,
     localisation TEXT
 );
+
+COPY vehicule_import(id, marque, modele, annee, energie, autonomie_km, immatriculation, etat, localisation)
+FROM 'C:/Users/pouru/Desktop/projett sql/data/vehicules_clara_2025.csv'
+DELIMITER ',' CSV HEADER;
+
+INSERT INTO station (ville, nb_bornes)
+SELECT DISTINCT localisation, 5 FROM vehicule_import;
+
+INSERT INTO vehicule (id_vehicule, marque, modele, annee, energie, autonomie_km, immatriculation, statut, id_station)
+SELECT vi.id, vi.marque, vi.modele, vi.annee, vi.energie, vi.autonomie_km, vi.immatriculation, vi.etat, s.id_station
+FROM vehicule_import vi
+JOIN station s ON s.ville = vi.localisation;
